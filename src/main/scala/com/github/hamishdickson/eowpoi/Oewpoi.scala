0528215ebdb30029fa7135fbd5109da636c8dad3
hamishdickson <hamish.dickson@gmail.com>
2016-09-15 20:54:06 +0100

    hmmm free is hard

--- src/main/scala/com/github/hamishdickson/eowpoi/Oewpoi.scala
+++ src/main/scala/com/github/hamishdickson/eowpoi/Oewpoi.scala
@@ -10,4 +10,13 @@ object Oewpoi {
   type Poi[A] = Free[PoiA, A]
 
   def get[T](id: Int): Poi[Option[T]] = liftF[PoiA, Option[T]](Get[T](id))
+
+  import cats.{Id, ~>}
+
+  def impureCompiler: PoiA ~> Id = new (PoiA ~> Id) {
+    // impure stuff here
+     def apply[A](fa: PoiA[A]): Id[A] = fa match {
+       case Get(id) => Some(id)
+     }
+  }
 }
